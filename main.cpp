7402cebc0b03e36b83d47759a9b38e61e12fa99e
Farbkecks <98021172+Farbkecks@users.noreply.github.com>
2023-04-01 14:58:30 +0200

    v1 Card Modula

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
-#include "Map.h"
 #include <algorithm>
 #include <memory>
 
 using namespace std;
 
+enum Types{
+    HEIGHT,
+    STRENGE
+};
+
+struct Attribute{
+    Types type;
+    double value;
+    explicit Attribute(Types t, double x): type(t), value(x){}
+};
+
+struct Card{
+    Card(Attribute height, Attribute strenge){
+        card.push_back(height);
+        card.push_back(strenge);
+    }
+    vector<Attribute> card;
+};
+
+bool compare(Types t, Card one, Card two){
+    for(int i=0; one.card.size(); i++){
+        if(one.card.at(i).type != t) continue;
+        return one.card.at(i).value > two.card.at(i).value;
+    }
+}
+
 int main() {
 
-    Map map(4);
-    map.show();
+    Card one(Attribute(HEIGHT, 2), Attribute(STRENGE, 3));
+    Card two(Attribute(HEIGHT, 1), Attribute(STRENGE, 6));
 
-    cout << "--------------------------------" << endl;
-
-    for(auto i: map) {
-        cout << i << endl;
-    }
-
-    cout << "--------------------------------" << endl;
-
-    Map::Iterator ptr(Coordinate(1,1), map);
-    cout << *ptr << endl;
-    auto neighbours = Map::get_neighbours(ptr);
-    for(auto i: neighbours){
-        cout << map.getReference(i) << endl;
-    }
+    cout << compare(HEIGHT, one, two) << endl;
+    cout << compare(STRENGE, one, two) << endl;
 
     return 0;
 }
