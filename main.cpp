99c962f376e33701c7ab9665b9a4bfd82452db06
Fabian A” <>
2023-04-28 13:38:22 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,60 +1,79 @@
-#include <iostream>
-#include <algorithm>
-#include <memory>
-#include <map>
+#include <Python.h>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include "iostream"
+#include <vector>
 
-using namespace std;
+//using namespace std;
 
-enum TypesEnum{
-    HEIGHT,
-    STRENGTH,
-    INTELLIGENCE
-};
 
-class CardBase{
-protected:
-    CardBase() = default;
-public:
-    template<TypesEnum T>
-    bool compare(CardBase const & other) const{
-        return getProperty<T>() > other.getProperty<T>();
+std::string
+file_contents(const std::filesystem::path& path)
+{
+    // Sanity check
+    if (!std::filesystem::is_regular_file(path))
+        return { };
+
+    // Open the file
+    // Note that we have to use binary mode as we want to return a string
+    // representing matching the bytes of the file on the file system.
+    std::ifstream file(path, std::ios::in | std::ios::binary);
+    if (!file.is_open())
+        return { };
+
+    // Read contents
+    std::string content{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+
+    // Close the file
+    file.close();
+
+    return content;
+}
+
+void print_container(const std::vector<int>& c)
+{
+    for (int i : c)
+        std::cout << i << " ";
+    std::cout << '\n';
+}
+
+int
+main(int argc, char *argv[])
+{
+
+    //Test
+    std::vector<int> x = {1,2};
+    print_container(x);
+
+    x.insert(x.begin()+x.size(), 3);
+    x.insert(x.begin()+x.size(), 3);
+    x.insert(x.begin()+x.size(), 3);
+
+    print_container(x);
+
+    return 0;
+    auto generatePairs = [](int const MAX = 8){
+        int row = 0;
+        int colume = 0;
+        std::vector<std::pair<int,int>> resulte;
+        resulte.resize(MAX);
+        for(auto & i: resulte){
+            if((int) MAX / 2 == colume){
+                row++;
+                colume = 0;
+            }
+            i = std::pair<int,int>(row,colume++);
+        }
+        return resulte;
+    };
+
+
+    for(auto [x,y] : generatePairs()){
+        std::cout << x << "   " << y << std::endl;
     }
 
-    template<TypesEnum T>
-    double getProperty() const
-    {
-        if(card.count(T) == 0)
-            return 0.0;
-        return card.at(T);
-    }
 
-    template<TypesEnum T>
-    void setProperty(double value)
-    {
-        card[T] = value;
-    }
-
-protected:
-    map<TypesEnum, double> card;
-};
-
-
-struct Card: public CardBase{
-    Card(double h, double s, double i){
-        setProperty<HEIGHT>(h);
-        setProperty<STRENGTH>(s);
-        setProperty<INTELLIGENCE>(i);
-    }
-};
-
-int main() {
-
-    Card one(1,5,3);
-    Card two(5,2,9);
-
-    cout << one.compare<HEIGHT>(two) << endl;
-    cout << one.compare<STRENGTH>(two) << endl;
-    cout << one.compare<INTELLIGENCE>(two) << endl;
 
     return 0;
 }
